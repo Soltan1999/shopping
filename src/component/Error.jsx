232c0a46b6129380662a961fdd8fb1897c9d5319
Soltan1999 <soltanabdurahmanov99@gmail.com>
2024-02-20 21:52:23 +0400

    Commit

---
+++ src/component/Error.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div className='bg-danger' style={{height:659}}>
+      <h1 className='text-center' style={{fontSize:"250px"}}>404</h1>
+      <h3 className='text-center' style={{fontSize:"70px",fontFamily:"-moz-initial"}}>Page not found</h3>
+    </div>
+  )
+}
+
+export default Error
